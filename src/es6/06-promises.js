211534add4a8c42f27c78335ecc5536ec6a73de7
Francisco Gomez <pgomezdev@gmail.com>
2022-10-15 12:59:46 -0500

    V_0.7.0 promise example

---
+++ src/es6/06-promises.js
@@ -0,0 +1,15 @@
+const myFunction = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("It was resolved :D!!");
+    } else {
+      reject("It was rejected >:(");
+    }
+  });
+};
+
+myFunction()
+  .then(response => console.log(response)) //? If the condition is true 
+  .catch(err => console.log(err)); //? If the condition is false
+
+//! This will return "It was resolved :D!!" in the case that te condition that we are evaluating is true, and it will return It was rejected >:( if the condition is false
