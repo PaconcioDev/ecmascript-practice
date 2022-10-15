e8251b830f0d6d93a4b8540e8f50a0a26b94f04c
Francisco Gomez <pgomezdev@gmail.com>
2022-10-15 12:50:12 -0500

    V_0.6.0 object literals example

---
+++ src/es6/05-object-literals.js
@@ -0,0 +1,13 @@
+//* Enhanced object literals
+
+function newUser(user, age, country, uId) {
+  return {
+    user, //? You don't have to put a value like in id property if the name is the same as the parameter name
+    age,
+    country,
+    id: uId,
+  };
+}
+
+console.log(newUser("Paconcio", 18, "CO", 111))
+//! This will return { user: 'Paconcio', age: 18, country: 'CO', id: 111 }
