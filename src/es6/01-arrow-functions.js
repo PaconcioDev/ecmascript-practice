04fcae48abd29aa40757222b2f04a937226e1575
Francisco Gomez <pgomezdev@gmail.com>
2022-10-15 10:18:14 -0500

    V_0.2.0

---
+++ src/es6/01-arrow-functions.js
@@ -0,0 +1,17 @@
+//* ES5
+function exampleFunction(num) {
+  return num * num;
+}
+
+//* ES6
+const arrowFunction = (num) => {
+  return num * num;
+};
+
+//* This syntaxis is used when you want to return immediatly, so you don't need to use () for the parameters if theres only one, or {} for the body of the function 
+
+const exampleArrowFunction = num => num * num;
+console.log(exampleArrowFunction(2)) //! This show the number 4 in the console
+
+const example2 = (num1, num2) => num1 * num2;
+console.log(example2(2, 3)) //! This show the number 6 in the console
