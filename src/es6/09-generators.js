dbaae2129dbbae0472bab2053fac5dc2a0c92234
Francisco Gomez <pgomezdev@gmail.com>
2022-10-26 10:52:09 -0500

    V_0.11.2 the last one was (07-classes)

--- src/es6/09-generators.js
+++ src/es6/09-generators.js
@@ -1,10 +1,13 @@
+const myArray = ["Paco", "David", "Majo", "Jhonny", "Kano"];
+
+//* To identify our generators de function keyword has to have "*" like in the example
 function* iterate(arr) {
   for (let value of arr) {
-    yield value;
+    yield value; //? The keyword yield is used to return each of our values
   }
 }
 
-const it = iterate(["Paco", "David", "Majo", "Jhonny", "Kano"]);
+const it = iterate(myArray);
 console.log(it.next().value); //! Paco
 console.log(it.next().value); //! David
 console.log(it.next().value); //! Majo
@@ -13,3 +16,4 @@ console.log(it.next().value); //! Kano
 console.log(it.next().value); //! undefined
 console.log(it.next().value); //! undefined
 
+//! The next() method returns an object with value and done properties, value represents the displayed value, and done indicates whether or not the iterator looped through all of its values.
--- src/es6/10-set-add.js
+++ src/es6/10-set-add.js
@@ -3,4 +3,4 @@ const list = new Set();
 list.add("item 1");
 list.add("item 2").add("item 3");
 
-console.log(list)
+console.log(list) //! This will return "Set(3) { 'item 1', 'item 2', 'item 3' }"
