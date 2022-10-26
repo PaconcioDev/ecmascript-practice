401057d6d8833e650d24a6f870ac76c03988a7d3
Francisco Gomez <pgomezdev@gmail.com>
2022-10-26 17:14:45 -0500

    V_0.17.0 ES12 not complete because theres a problem on promise.any

---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,7 @@
+const myString = "JS is a wonderfull programming language";
+
+console.log(myString); //! JS is a wonderfull programming language
+
+const replacedStr = myString.replace("JS", "TypeScript");
+
+console.log(replacedStr); //! TypeScript is a wonderfull programming language
